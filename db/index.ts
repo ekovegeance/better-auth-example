6ab3c82071890080cb1ead165bc69ce7bbeb875e
Eko <ekovegeance7@gmail.com>
2025-07-01 21:24:51 +0700

    feat: update database client to Neon and adjust session handling

--- db/index.ts
+++ db/index.ts
@@ -1,4 +1,8 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import {drizzle} from "drizzle-orm/neon-http";
+import {neon} from "@neondatabase/serverless";
+import {config} from "dotenv";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+config({path: ".env"});
+const posgres = neon(process.env.DATABASE_URL!);
+export const db = drizzle({client: posgres});
