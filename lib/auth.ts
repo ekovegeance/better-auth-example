9bba6da77d7ff78337764083c8f03fc6e149f9a0
Eko <ekovegeance7@gmail.com>
2025-07-01 22:04:44 +0700

    feat: add baseURL configuration for betterAuth

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,14 +6,18 @@ import {schema} from "@/db/schema";
 
 export const auth = betterAuth({
     secret: process.env.BETTER_AUTH_SECRET,
+    baseURL: process.env.BETTER_AUTH_URL,
+
     database: drizzleAdapter(db, {
         provider: "pg",
         schema: {
             ...schema,
         }
     }),
+
     emailAndPassword: {
         enabled: true,
     },
+
     plugins: [nextCookies()]
 })
