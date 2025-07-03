24a2efdf0393ccf9b6f07a6879442a49059cf87b
Eko <ekovegeance7@gmail.com>
2025-07-03 19:42:37 +0700

    fix: correct typo in database variable name and update module file reference

--- db/index.ts
+++ db/index.ts
@@ -4,5 +4,5 @@ import {neon} from "@neondatabase/serverless";
 import {config} from "dotenv";
 
 config({path: ".env"});
-const posgres = neon(process.env.DATABASE_URL!);
-export const db = drizzle({client: posgres});
+const postgres = neon(process.env.DATABASE_URL!);
+export const db = drizzle({client: postgres});
